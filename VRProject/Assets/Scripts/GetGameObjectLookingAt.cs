b97edae950e7ce95a9deb09df1a810b6abdba4b6
Fernando Ferrando Terradez <metrack92@gmail.com>
2017-02-15 12:11:40 +0000

    draft of movement and getting the object,s info
    First draft of the movement.
    
    Got unity movement and being able to get the object that i currently
    face with spacebar.

---
+++ VRProject/Assets/Scripts/GetGameObjectLookingAt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGameObjectLookingAt : MonoBehaviour {
+    private RaycastHit hit;
+    public GameObject eye;
+    public GameObject hitted_object;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+
+            if (Physics.Raycast(eye.transform.position, eye.transform.forward, out hit)) {
+                Debug.DrawRay(eye.transform.position, eye.transform.forward, Color.green);
+                hitted_object = hit.transform.gameObject;
+                Debug.Log(hit.transform.name);
+            }   
+        }
+	}
+}
