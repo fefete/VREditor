5aa138581efdd0c715ccdf8ee58b06ad65d2cbc4
Fernando Ferrando Terrádez <metrack92@gmail.com>
2017-02-20 10:10:06 +0000

    Added first sketch of the transform inspector.Still not functional

--- VRProject/Assets/Scripts/GetGameObjectLookingAt.cs
+++ VRProject/Assets/Scripts/GetGameObjectLookingAt.cs
@@ -6,19 +6,23 @@ public class GetGameObjectLookingAt : MonoBehaviour {
     private RaycastHit hit;
     public GameObject eye;
     public GameObject hitted_object;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public UpdateCollector pos;
+    public UpdateCollector rot;
+    public UpdateCollector sca;
+
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.Space)) {
 
             if (Physics.Raycast(eye.transform.position, eye.transform.forward, out hit)) {
                 Debug.DrawRay(eye.transform.position, eye.transform.forward, Color.green);
                 hitted_object = hit.transform.gameObject;
                 Debug.Log(hit.transform.name);
+
+                pos.newValues(hitted_object.transform.position, hitted_object);
+                pos.newValues(hitted_object.transform.rotation.eulerAngles, hitted_object);
+                pos.newValues(hitted_object.transform.localScale, hitted_object);
             }   
         }
 	}
---
+++ VRProject/Assets/Scripts/UpdateCollector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCollector : MonoBehaviour {
+
+    public InputField x;
+    public InputField y;
+    public InputField z;
+
+    public GameObject currentGameObject;
+
+    public void onValueChangePos(string temp)
+    {
+        currentGameObject.transform.position = new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text));
+    }
+    public void onValueChangeRot(string temp)
+    {
+        currentGameObject.transform.eulerAngles = new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text));
+    }
+    public void onValueChangeScale(string temp)
+    {
+        currentGameObject.transform.localScale = new Vector3(float.Parse(x.text), float.Parse(y.text), float.Parse(z.text));
+    }
+    public void newValues(Vector3 v, GameObject obj)
+    {
+        x.text = v.x.ToString();
+        y.text = v.y.ToString();
+        z.text = v.z.ToString();
+
+        currentGameObject = obj;
+    }
+}
